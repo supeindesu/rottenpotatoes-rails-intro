a022701f935391a2785f4369b68b1f1e55a63e8b
supeindesu <supeindesu@hotmail.com>
2015-11-15 18:23:46 +0000

    My_version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,35 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    #@movies = Movie.all
+    @all_ratings = Movie.ratings
+    @sort = params[:sort]
+    @ratings = params[:ratings]
+    #check deployment
+    if params[:ratings].nil? and session[:ratings].nil?
+      @ratings = @all_ratings
+      params[:ratings] = @all_ratings
+      session[:ratings] = @all_ratings
+      @movies = Movie.find_all_by_rating(@ratings.keys)
+    end
+    if params[:sort] == session[:sort] and params[:ratings] == session[:ratings]
+      if params[:sort].nil?
+        @ratings = params[:ratings]
+        @movies = Movie.find_all_by_rating(params[:ratings].keys)
+      else
+        @ratings = params[:ratings]
+        @movies = Movie.order(params[:sort]).find_all_by_rating(params[:ratings].keys)
+      end
+    else
+      if !params[:sort].nil?
+        session[:sort] = params[:sort]
+      end
+      if !params[:ratings].nil?
+        session[:ratings] = params[:ratings]
+      end
+      flash.keep
+      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
+    end
   end
 
   def new
